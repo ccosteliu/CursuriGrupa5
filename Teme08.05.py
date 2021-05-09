f4a2c81fa2d609bd4cc44efdfb135b80acf9446a
CristiANDRA <ccosteliu@gmail.com>
2021-05-09 09:21:15 +0300

    Teme

--- Teme08.05.py
+++ Teme08.05.py
@@ -22,44 +22,44 @@ print(text)
 
 # Tema 3
 
-# anul = int(input("Anul: "))
-#
-# if anul % 4 == 1:
-#     text = f"Acest an nu este bisect."
-# elif anul % 4 == 0:
-#     text = f"Acest an este bisect."
-# print(text)
+anul = int(input("Anul: "))
+
+if anul % 4 == 1:
+    text = f"Acest an nu este bisect."
+elif anul % 4 == 0:
+    text = f"Acest an este bisect."
+print(text)
 
 # Tema 4
 
-# numar = eval(input("Introduceti un nr: "))
-# if numar in range(1,10):
-#     print("Numarul este pozitiv si mai mic decat 10")
-# elif numar == 0:
-#     print("Numarul este 0.")
-# elif numar < 0:
-#     print(f'Numarul pozitiv este: {abs(numar)}.')
+numar = eval(input("Introduceti un nr: "))
+if numar in range(1,10):
+    print("Numarul este pozitiv si mai mic decat 10")
+elif numar == 0:
+    print("Numarul este 0.")
+elif numar < 0:
+    print(f'Numarul pozitiv este: {abs(numar)}.')
 
 #Tema 5
-# meniu = ""
 #
 # while meniu.lower() != "quit":
-#     meniu = input("> ")
-#     if meniu == "help":
-#         print("""
-# 1 – Afisare lista de cumparaturi
-# 2 – Adaugare element
-# 3 – Stergere element
-# 4 – Stergere lista de cumparaturi
-# 5 - Cautare in lista de cumparaturi""")
-#     elif meniu == "1":
-#         print("Afisare lista de cumparaturi")
-#     elif meniu == "2":
-#         print("Adaugare element")
-#     elif meniu == "3":
-#         print("Stergere element")
-#     elif meniu == "4":
-#         print("Stergere lista cumparaturi")
-#     elif meniu == "5":
-#         print("Cautare in lista de cumparaturi")
-#     else:print("Alegerea nu exista.Reincercati")
+meniu = ""
+    meniu = input("> ")
+    if meniu == "help":
+        print("""
+1 – Afisare lista de cumparaturi
+2 – Adaugare element
+3 – Stergere element
+4 – Stergere lista de cumparaturi
+5 - Cautare in lista de cumparaturi""")
+    elif meniu == "1":
+        print("Afisare lista de cumparaturi")
+    elif meniu == "2":
+        print("Adaugare element")
+    elif meniu == "3":
+        print("Stergere element")
+    elif meniu == "4":
+        print("Stergere lista cumparaturi")
+    elif meniu == "5":
+        print("Cautare in lista de cumparaturi")
+    else:print("Alegerea nu exista.Reincercati")
