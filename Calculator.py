62c1741773ca90f9aaa3ab693dcaf9320dab5e03
CristiANDRA <ccosteliu@gmail.com>
2021-05-27 11:41:50 +0300

    Calculator

---
+++ Calculator.py
@@ -0,0 +1,50 @@
+def get_input() -> (float,float,str):
+    a=float(input("a >> "))
+    operatie=input("operatie >> ")
+    b = float(input("b >> "))
+    return a, operatie, b
+
+
+def suma(a: float, b: float):
+    return a+b
+
+
+def subt(a: float, b: float):
+    return a-b
+
+
+def mult(a: float, b: float):
+    return a*b
+
+
+def div(a: float, b: float):
+    try:
+        return a/b
+    except ZeroDivisionError:
+        return 0, "Division by 0 - error"
+
+
+def calculator():
+    alpha, operation, beta  = get_input()
+    variabila = ""
+    if operation == "+":
+        variabila = suma(alpha, beta)
+        print(variabila)
+    elif operation == "-":
+        variabila=subt(alpha, beta)
+        print(variabila)
+    elif operation == "*":
+        variabila = mult(alpha, beta)
+        print(variabila)
+    elif operation == "/":
+        variabila = div(alpha, beta)
+        print(variabila)
+    return variabila
+
+
+while True:
+    calculator()
+
+
+
+
